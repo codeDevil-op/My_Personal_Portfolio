89ab32272dc8c19e7eb5e766aed815ad07fef8a0
NightMare <rahmanishfaq146@gmail.com>
2024-10-06 21:30:58 +0500

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import './app.css'
+import './App.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Tooltip as ReactTooltip } from 'react-tooltip'
